bc40efc74b8c8fe26ab9a2d2d03f8ec3c1fe9c68
unknown <vidya9955297836@gmail.com>
2024-07-30 21:41:15 +0530

    Initial Commit

---
+++ src/ui/Logo.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { useDarkMode } from "../context/DarkModeContext";
+
+const StyledLogo = styled.div`
+  text-align: center;
+`;
+
+const Img = styled.img`
+  height: 9.6rem;
+  width: auto;
+`;
+
+const ImageText = styled.p`
+  letter-spacing: 2px;
+  font-family: monospace;
+`;
+
+function Logo() {
+  const { isDarkMode } = useDarkMode();
+
+  const src = isDarkMode ? "/Hotel-Logo.png" : "/Hotel-Logo.png";
+
+  return (
+    <StyledLogo>
+      <Img src={src} alt="Logo" />
+      <ImageText>NITA Buddy</ImageText>
+    </StyledLogo>
+  );
+}
+
+export default Logo;
